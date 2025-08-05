41fa7095a8592e54391ea5ca38b7467c6cddc183
Anthony Usoro <33380362+tiusoro@users.noreply.github.com>
2025-08-05 14:32:36 +0100

    Update outputs.tf
    edited out output

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
-output "instance_ami" {
-  value = aws_instance.web.ami
-}
+#output "instance_ami" {
+#  value = aws_instance.web.ami
+#}
 
 #output "instance_arn" {
 #  value = aws_instance.web.arn
