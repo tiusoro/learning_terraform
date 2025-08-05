c7d4232157b545cfa934cdc47b0356dfc2e6dc46
Anthony Usoro <33380362+tiusoro@users.noreply.github.com>
2025-08-05 14:37:40 +0100

    Update main.tf
    allow ssh into the server

--- main.tf
+++ main.tf
@@ -35,6 +35,16 @@ resource "aws_security_group" "blog" {
 vpc_id = data.aws_vpc.default.id
 }
 
+resource "aws_security_group_rule" "blog_ssh_in" {
+  type        = "ingress"
+  from_port   = 22
+  to_port     = 22
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+security_group_id = aws_security_group.blog.id
+}
+
 resource "aws_security_group_rule" "blog_http_in" {
   type        = "ingress"
   from_port   = 80
