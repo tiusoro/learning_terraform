ed4a07e441eee3ed5cb91aa1c97b8a2b9a57efeb
Anthony Usoro <33380362+tiusoro@users.noreply.github.com>
2025-08-07 14:18:03 +0100

    Update main.tf
    create dev vpc

--- main.tf
+++ main.tf
@@ -21,7 +21,6 @@ module "vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
@@ -53,7 +52,6 @@ module "blog_sg" {
 
   ingress_rules = ["http-80-tcp", "https-443-tcp", "ssh-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-
   egress_rules = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
