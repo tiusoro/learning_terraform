ccd562ebeb092c989964a0a19a57b334403ac109
Anthony Usoro <33380362+tiusoro@users.noreply.github.com>
2025-08-07 12:02:18 +0100

    Update main.tf
    edited vpc to customized address

--- main.tf
+++ main.tf
@@ -13,8 +13,22 @@ data "aws_ami" "app_ami" {
 
   owners = ["979382823631"] # Bitnami
 }
-data "aws_vpc" "default" {
-  default = true
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
 
 resource "aws_instance" "blog" {
@@ -32,9 +46,10 @@ vpc_security_group_ids = [module.blog_sg.security_group_id]
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.3.0"
-  name = "blog_new"
 
-  vpc_id = data.aws_vpc.default.id
+  name = "blog"
+  vpc_id = module.vpc.public_subnet[0]
+
   ingress_rules = ["http-80-tcp", "https-443-tcp", "ssh-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
